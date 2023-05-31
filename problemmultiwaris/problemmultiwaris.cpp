7420eb514d90500d8281e8af38f4b808363cec6c
foosaan <fauzan4297@gmail.com>
2023-05-31 11:00:28 +0700

    Add project files.

---
+++ problemmultiwaris/problemmultiwaris.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class orang {
+public:
+	int umur;
+
+	orang(int pUmur) :
+		umur(pUmur)
+	{
+		cout << "orang dibuat dengan umur" << umur << "\n" endl;
+	}
+};
+
+class pelajar : public orang {
+public:
+	pekerja(int pUmur) :
+		orang(pUmur)
+	{
+		cout << "pekerja dibuat\n" << endl;
+	}
+};
+
+class pelajar : public orang {
+public:
+
+};
