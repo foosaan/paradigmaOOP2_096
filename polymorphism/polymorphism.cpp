ce1b2714e45539c709bb547a88a0770cab796af3
foosaan <fauzan4297@gmail.com>
2023-05-31 19:55:40 +0700

    membuat class joko dengan turunan class seseorang

--- polymorphism/polymorphism.cpp
+++ polymorphism/polymorphism.cpp
@@ -3,5 +3,16 @@ using namespace std;
 
 class seseorang {
 public:
+	virtual void pesan() = 0;
+	//virtual void pesan(){
+		//cout<<"pesan dari seseorang"<<endl;
+	//}
+	//virtual void cetak() = 0;
+	virtual void cetak() {
+
+	}
+};
+
+class joko : public seseorang {
 
 };
