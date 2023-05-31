2ea72eb27f1e068711eaf77e9fd858a7ea9a6a83
foosaan <fauzan4297@gmail.com>
2023-05-31 20:30:09 +0700

    menampilkan class seseorang

--- polymorphism/polymorphism.cpp
+++ polymorphism/polymorphism.cpp
@@ -29,3 +29,19 @@ public:
 		cout << "pesan dari lia" << endl;
 	}
 };
+
+int main() {
+	seseorang* orang;
+	joko a;
+	lia b;
+
+	orang = &a;
+	orang->pesan();
+	orang = &b;
+	orang->pesan();
+	//a.seseorang::pesan(); //memanggil method yang berada di class orang tua
+	//b.seseorang::pesan();
+
+	return 0;
+
+}
